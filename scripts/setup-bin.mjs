f8fd57dc428ece39d8d4e26c0ac78472aa9ffdc5
barathrajd <barathraj.uidev@gmail.com>
2025-12-20 14:37:31 +0530

    chore: Remove package.json generation and README.md copying from setup script.

--- scripts/setup-bin.mjs
+++ scripts/setup-bin.mjs
@@ -1,38 +1,13 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-const binPath = path.resolve('dist/bin/task-cli.js');
-const packageJsonPath = path.resolve('dist/package.json');
-const packageJsonVersion = JSON.parse(fs.readFileSync('package.json'));
+const binPath = path.resolve('bin/task-cli.js');
 
 const content = `#!/usr/bin/env node
-require('../task-cli.js');
-`;
-
-const packageJsonContent = `
-{
-  "name": "${packageJsonVersion.name}",
-  "version": "${packageJsonVersion.version}",
-  "type": "module",
-  "exports": {
-    ".": {
-      "types": "./index.d.ts",
-      "import": "./index.js"
-    }
-  },
-  "types": "./index.d.ts",
-  "bin": { "task-cli": "bin/task-cli.js" }
-}
+require('../dist/task-cli.js');
 `;
 
 fs.mkdirSync(path.dirname(binPath), { recursive: true });
 fs.writeFileSync(binPath, content);
 fs.chmodSync(binPath, 0o755);
 console.log('✔ bin file created');
-
-fs.writeFileSync(packageJsonPath, packageJsonContent);
-fs.chmodSync(packageJsonPath, 0o755);
-console.log('✔ package.json file created');
-
-fs.copyFileSync(path.resolve('README.md'), path.resolve('dist/README.md'));
-console.log('✔ README.md copied');
